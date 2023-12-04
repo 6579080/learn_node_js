6769bb004c3757f9512a76e3bf616d884369d5ab
Ulad <6579080@gmail.com>
2023-12-04 17:15:13 +0300

    2

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
-const http = require('http');
-const EventEmitter = require('events');
-// const {router} = require("express/lib/application");
+
 const PORT = process.env.PORT || 5000;
-const Router = require('./framework/Router');
+// const Router = require('./framework/Router');
 const Application = require('./framework/Application');
-// const emitter = new EventEmitter();
+const userRouter = require('./src/user-router');
 
 const app = new Application();
 
 
-const router = new Router();
 
-router.get('/users', (req, res) => {
-    res.end('you send request to /users')
-})
 
-router.get('/posts', (req, res) => {
-    res.end('you send request to /posts')
-})
 
-app.addRouter(router);
+app.addRouter(userRouter);
 
-// const server = http.createServer(
-//
-//     // res.writeHead(200, {
-//     //     'Content-type': 'application/json'
-//     // })
-//     // // res.end('<h1>1</h1>сервер работает! gfffffvv');
-//     // if (req.url === '/users') {
-//     //     return res.end(JSON.stringify([
-//     //         {id: 1, name: 'NAME'}
-//     //     ]))
-//     // }
-//     // if (req.url === '/posts') {
-//     //     return res.end('POSTS')
-//     // }
-//     // res.end(req.url)
-//  )
+
 app.listen(PORT, () => console.log(`server started on PORT ${PORT}`));
 
 
